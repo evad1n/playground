147e9d184e13ef3678f067ca7d26748685b42906
Will Dickinson <willdickinson98@gmail.com>
2021-02-05 18:42:54 -0700

    some js fractal stuff, and did some fizzbuzz multithreading with goroutines

---
+++ go/fizzbuzz/fizzbuzz.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+const limit = 100000000
+
+/* The goroutines definitely run faster
+Got faster as CPU warmed up or something, I could avg them but who cares
+standard (10^8): ~5s
+goroutines (10^8 with 1 goroutines): 5.126s
+goroutines (10^8 with 2 goroutines): 3.591s
+goroutines (10^8 with 4 goroutines): 3.065s
+goroutines (10^8 with 8 goroutines): 2.783s I have 8 cores
+goroutines (10^8 with 20 goroutines): 2.679s
+goroutines (10^8 with 50 goroutines): 2.666s
+
+Clearly some diminishing returns
+*/
+
+func main() {
+	goroutines(limit, 1)
+	// standard(limit)
+}
+
+func standard(num int) {
+	output := make([]string, num)
+	for i := 0; i < num; i++ {
+		output[i] = fizzbuzz(i)
+	}
+}
+
+func goroutines(num int, numThreads int) {
+	var wg sync.WaitGroup
+
+	chunkSize := num / numThreads
+
+	output := make([]string, num)
+	for i := 0; i < numThreads; i++ {
+		wg.Add(1)
+		go fizzBuzzThread(&wg, i*chunkSize, (i*chunkSize)+chunkSize, &output)
+	}
+	wg.Wait()
+	// for _, line := range output {
+	// 	fmt.Println(line)
+	// }
+}
+
+func fizzBuzzThread(wg *sync.WaitGroup, start int, end int, out *[]string) {
+	defer wg.Done()
+	for i := start; i < end; i++ {
+		(*out)[i] = fizzbuzz(i)
+	}
+}
+
+func fizzbuzz(i int) string {
+	n := 0
+	if i%3 == 0 {
+		n++
+	}
+	if i%5 == 0 {
+		n += 2
+	}
+
+	switch n {
+	case 1:
+		return "Fizz"
+	case 2:
+		return "Buzz"
+	case 3:
+		return "FizzBuzz"
+	default:
+		return fmt.Sprint(i)
+	}
+}
--- javascript/fractals/fractal.js
+++ javascript/fractals/fractal.js
@@ -1,3 +1,92 @@
 function setup() {
-    background(0);
+    createCanvas(windowWidth, windowHeight);
+    // noLoop();
+    frameRate(5);
+
+    angle = radians(30);
+    center = createVector(width / 2, height / 2);
+    ratio = 3 / 5;
+    scale = 10;
+    frameTimeout = 10;
+    lastFrame = 0;
+}
+
+function mouseMoved() {
+    let mouse = createVector(mouseX, mouseY);
+    angle = degrees(mouse.sub(center).heading());
+    scale = mouseX / width * 100;
+    // print(frameCount);
+    // redraw();
+}
+
+function draw() {
+    background(255);
+    stroke(0);
+    strokeWeight(1);
+    noFill();
+    // drawCircle(width / 2, height / 2, width / 2);
+    // drawSquare(width / 2, height / 2, width / 2);
+    // drawTriangle(0, height, width / 2, 0, width, height);
+    // cantor(9, height / 2, width);
+    branch(createVector(width / 2, height), createVector(0, -height * 1 / 4), angle, 3);
+    // branch(createVector(width / 4, height), createVector(0, -height * 1 / 4), angle);
+    // branch(createVector(width * 3 / 4, height), createVector(0, -height * 1 / 4), angle);
+}
+
+function branch(pos, vec, angle, bf) {
+    strokeWeight(vec.mag() / 50);
+    let nextPos = p5.Vector.add(pos, vec);
+    line(pos.x, pos.y, nextPos.x, nextPos.y);
+    if (vec.mag() > 5) {
+        for (let i = 0; i < bf; i++) {
+            let a = (i * angle / (bf - 1)) + (-angle / 2);
+            branch(nextPos, vec.copy().rotate(radians(a)).setMag(vec.mag() * ratio), angle, bf);
+        }
+    }
+}
+
+function cantor(x, y, len) {
+    if (len >= 1) {
+        line(x, y, x + len, y);
+        y += 20;
+        cantor(x, y, len / 3);
+        cantor(x + len * 2 / 3, y, len / 3);
+    }
+}
+
+function drawCircle(x, y, radius) {
+    circle(x, y, radius);
+    if (radius > scale) {
+        drawCircle(x + radius / 2, y, radius / 2);
+        drawCircle(x - radius / 2, y, radius / 2);
+        drawCircle(x, y + radius / 2, radius / 2);
+        drawCircle(x, y - radius / 2, radius / 2);
+    }
+}
+
+function drawSquare(x, y, size) {
+    square(x, y, size);
+    if (size > scale) {
+        drawSquare(x + size / 2, y, size / 2);
+        drawSquare(x - size / 2, y, size / 2);
+        drawSquare(x, y + size / 2, size / 2);
+        drawSquare(x, y - size / 2, size / 2);
+    }
+}
+
+function drawTriangle(x1, y1, x2, y2, x3, y3) {
+    // fill(255 - (255 * (x3 - x1) / width));
+    triangle(x1, y1, x2, y2, x3, y3);
+    let center = createVector(x1 + ((x3 - x1) / 2), (y2 + ((y1 - y2) / 2)));
+    if (x3 - x1 > scale) {
+        // drawTriangle(x1, y1, x2 / 2, height - (height - y2) / 2, x3 - (x3 - x1) / 2, y3);
+        // drawTriangle(x1 + (x3 - x1) / 2, y3, width - ((width - x2) / 2), height - (height - y2) / 2, x3, y3);
+        // drawTriangle(x2 / 2, height - (height - y2) / 2, x2, y2, width - (x2 / 2), height - (height - y2) / 2);
+        let midLeft = createVector(x1 + ((x2 - x1) / 2), y2 + ((y1 - y2) / 2));
+        let midRight = createVector(x2 + ((x3 - x2) / 2), midLeft.y);
+        let midBot = createVector(x1 + ((x3 - x1) / 2), y3);
+        drawTriangle(x1, y1, midLeft.x, midLeft.y, midBot.x, midBot.y);
+        drawTriangle(midLeft.x, midLeft.y, x2, y2, midRight.x, midRight.y);
+        drawTriangle(midBot.x, midBot.y, midRight.x, midRight.y, x3, y3);
+    }
 }
